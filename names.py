4f6fe50367b0aec2c17852968871b9c1357e6a6d
Aarron Walter <awalter@Tiny-Computer.local>
2015-11-18 20:57:03 -0500

    Adding a bunch of little practice scripts.

---
+++ names.py
@@ -0,0 +1,6 @@
+name = input("What's your name? ")
+
+if name == "Aarron":
+	print("Hey, " + name)
+else:
+	print(name + "you da man")
---
+++ strformat.py
@@ -0,0 +1,4 @@
+name = "Aarron"
+treehouse = "Tree"+"house"
+email_greeting = "{} loves {}".format(name, treehouse)
+print(email_greeting)
